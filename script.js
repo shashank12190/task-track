96eba143d901836a17228d9110609c37b759c931
shashank <pshashank1729@gmail.com>
2025-07-09 17:13:00 +0530

    final commit

--- script.js
+++ script.js
@@ -2,4 +2,7 @@
   This is just a sample project for my Git crash course. It doesn't do anything.
 */
 
-console.log('Task Tracker Running...');
+console.log("Task Tracker Running...");
+console.log("Task Tracker Running...");
+console.log("Task Tracker Running...");
+console.log("Task Tracker Running...");
